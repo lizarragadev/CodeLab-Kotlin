bef9e48ecf95747aee4138c49c9ba42eec8e34f7
Gustavo Lizarraga <lizarraga.gux@gmail.com>
2017-08-24 07:14:35 -0400

    Cambios iniciales Demo

--- app/src/main/kotlin/com/miramicodigo/pokekotlin/model/PokemonKt.kt
+++ app/src/main/kotlin/com/miramicodigo/pokekotlin/model/PokemonKt.kt
@@ -1,11 +1,9 @@
 package com.miramicodigo.pokekotlin.model
 
-import java.io.Serializable
-
 /**
  *
  * @author Gustavo Lizarraga
  * @version 1.1
  */
-class PokemonKt(var nombre: String?, var cp: String?, var imagen: Int) : Serializable
+
 
--- app/src/main/kotlin/com/miramicodigo/pokekotlin/ui/DetalleKtActivity.kt
+++ app/src/main/kotlin/com/miramicodigo/pokekotlin/ui/DetalleKtActivity.kt
@@ -3,21 +3,10 @@ package com.miramicodigo.pokekotlin.ui
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-import com.miramicodigo.pokekotlin.R
-import com.miramicodigo.pokekotlin.model.PokemonKt
-import kotlinx.android.synthetic.main.activity_detalle_kt.*
-
 class DetalleKtActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detalle_kt)
-
-        val pokemon = intent.getSerializableExtra("pokemon") as PokemonKt
-
-        tvDetalleKtNombre!!.setText(pokemon.nombre)
-        tvDetalleKtCP!!.text = "CP " + pokemon.cp
-        ivDetalleKtImagen!!.setImageResource(pokemon.imagen)
 
     }
 }
